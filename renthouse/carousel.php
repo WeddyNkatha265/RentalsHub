bde8aef37013a7134a03b57ece3ca0fb89a94e0a
Weddy Nkatha <weddynkatha265@gmail.com>
2024-06-11 17:14:40 +0300

    waah

--- renthouse/carousel.php
+++ renthouse/carousel.php
@@ -1,99 +1,11 @@
- <!--CAROUSEL-->
- <div
-      id="carouselExampleFade"
-      class="carousel slide carousel-fade"
-      data-bs-ride="carousel"
-    >
-      <div class="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="0"
-          class="active"
-          aria-current="true"
-          aria-label="Slide 1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="1"
-          aria-label="Slide 2"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide-to="2"
-          aria-label="Slide 3"
-        ></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="2000">
-          <div class="overlay"></div>
-          <img
-            src="../images/img3.png"
-            class="d-block w-100"
-            alt="Slide 1"
-            style="height: 550px"
-          />
-          <div
-            class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center bottom-0"
-          >
-            <h1
-              class="bg-dark bg-opacity-50 py-2 px-4 fw-bold"
-              style="font-size: 4vw"
-            >
-              <b style="color: #00ffff">Waste Wisely, </b>Live Greenly
-            </h1>
-            <h4 class="bg-dark bg-opacity-50 py-2 px-4 fw-bold">
-              Providing sustainable waste management solutions in Africa
-            </h4>
-          </div>
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <div class="overlay"></div>
-          <img
-            src="../images/img2.jpg"
-            class="d-block w-100"
-            alt="Slide 2"
-            style="height: 550px"
-          />
-          <div
-            class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center bottom-0"
-          >
-            <h1
-              class="bg-dark bg-opacity-50 py-2 px-4 fw-bold"
-              style="font-size: 4vw"
-            >
-              Don't Trash It, <b style="color: #00ffff">Recycle It!</b>
-            </h1>
-            <h4 class="bg-dark bg-opacity-50 py-2 px-4 fw-bold">
-              Converting waste materials into reusable objects.
-            </h4>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div class="overlay"></div>
-          <img
-            src="../images/img3.jpeg"
-            class="d-block w-100"
-            alt="Slide 3"
-            style="height: 550px"
-          />
-          <div
-            class="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center bottom-0"
-          >
-            <h1
-              class="bg-dark bg-opacity-50 py-2 px-4 fw-bold"
-              style="font-size: 4vw"
-            >
-              <b style="color: #00ffff">Optimize Waste,</b> Maximize Savings
-            </h1>
-            <h4 class="bg-dark bg-opacity-50 py-2 px-4 fw-bold">
-              Providing actionable recommendations to reduce waste, enhance
-              recycling rates.
-            </h4>
-          </div>
-        </div>
+ 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="images/carousel.png" alt="RentHouse" style="width:100%; height: 50%;">
       </div>
     </div>
-    <!--END OF CAROUSEL-->
+  </div>
